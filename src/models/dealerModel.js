c5e2b60eb46811585f7739ec9f671c9455f1cce1
subhram260 <subhram336@gmail.com>
2025-10-04 03:21:15 +0530

    table name user

--- src/models/dealerModel.js
+++ src/models/dealerModel.js
@@ -1,13 +1,13 @@
 import pool from "../config/db.js";
 
-const getAllDealers = async () => {
-    const query = 'SELECT "dealer_id","Name","Email","Password","Location","Phone","Role","Status" FROM spp."Dealers"';
+const getAllUsers = async () => {
+    const query = 'SELECT "user_id","Name","Email","Password","Location","Phone","Role","Status" FROM spp."Users"';
     const result = await pool.query(query);
     return result.rows;
 };
 
 const getDealerById = async (id) => {
-    const query = 'SELECT "dealer_id","Name","Email","Password","Location","Phone","Role","Status" FROM spp."Dealers" WHERE "dealer_id" = $1';
+    const query = 'SELECT "user_id","Name","Email","Password","Location","Phone","Role","Status" FROM spp."Users" WHERE "user_id" = $1';
     const values = [id];
     const result = await pool.query(query, values);
     return result.rows[0];
@@ -15,14 +15,14 @@ const getDealerById = async (id) => {
 
 // const createDealer = async (data) => {
 //     const { name, location } = data;
-//     const query = 'INSERT INTO spp.Dealers (Name, Location) VALUES ($1, $2) RETURNING *';
+//     const query = 'INSERT INTO spp.Users (Name, Location) VALUES ($1, $2) RETURNING *';
 //     const values = [name, location];
 //     const result = await pool.query(query, values);
 //     return result.rows[0];
 // };
 
 export {
-    getAllDealers,
+    getAllUsers,
     getDealerById
 //     createDealer
 };
